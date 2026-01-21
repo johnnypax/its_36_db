5c5379a72894e2b05b532f3d28bafa635dc8e56f
Giovanni Pace <g.pace@braintechinnovations.it>
2026-01-21 12:01:33 +0100

    Aggiunto DDL

--- lez01_comandi_ddl.sql
+++ lez01_comandi_ddl.sql
@@ -1,3 +1,4 @@
+-- DDL
 -- DROP DATABASE IF EXISTS lez01_comandi_ddl;
 CREATE DATABASE lez01_comandi_ddl;
 USE lez01_comandi_ddl;
@@ -9,6 +10,11 @@ CREATE TABLE Persona(
     telefono VARCHAR(255)
 );
 
+ALTER TABLE Persona ADD COLUMN email VARCHAR(255) AFTER cod_fis;
+ALTER TABLE Persona DROP COLUMN email;
+ALTER TABLE Persona MODIFY COLUMN cod_fis VARCHAR(14);
+
+-- DML
 INSERT INTO Persona (nome, cognome, cod_fis, telefono) VALUES
 ('Marco', 'Rossi', 'RSSMRC80A01H501U', '3331234567'),
 ('Luca', 'Bianchi', 'BNCLCU85B15F205Z', '3209876543'),
@@ -20,6 +26,8 @@ INSERT INTO Persona (nome, cognome, cod_fis, telefono) VALUES
 ('Chiara', 'Romano', 'RMNCHR88H12F205T', '3923344556'),
 ('Francesco', 'De Luca', 'DLCFNC91L20H501R', '3507788991'),
 ('Alessia', 'Greco', 'GRCLSS86M05L219W', '3661122445');
+
+
 
 
 SELECT * FROM Persona;
