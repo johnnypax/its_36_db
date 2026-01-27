e7cf1ef24cfec2807cb81bce4117b357f56fb78d
Giovanni Pace <g.pace@braintechinnovations.it>
2026-01-27 10:07:46 +0100

    Sorgente aggiornato

--- lez04_negozi.sql
+++ lez04_negozi.sql
@@ -14,4 +14,24 @@ INSERT INTO Persona(nome, cognome, email) VALUES
 ("Marika", "Mariko", "mar@mar.com"),
 ("Mario", "Rossi", "mar@ros.com");
 
+CREATE TABLE Carta(
+	cartaID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    codice VARCHAR(10) NOT NULL UNIQUE,
+    negozio VARCHAR(250) NOT NULL,
+    personaRIF INTEGER NOT NULL,
+    FOREIGN KEY(personaRIF) REFERENCES Persona(personaID)
+);
+
+INSERT INTO Carta(codice, negozio, personaRIF) VALUES
+("CC001", "Conad", 1),
+("CC002", "Conad", 2),
+("CC003", "Conad", 3),
+("PP01", "Coop", 1),
+("PP02", "Coop", 2),
+("TT001", "Tigotà", 2);
+
+
+
+
 SELECT * FROM Persona;
+SELECT * FROM Carta;
